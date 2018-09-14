c799226085038134140b6130998e57d36da0be67
stakah <stakah@gmail.com>
2018-09-13 22:23:13 -0300

    Adding d3.js test page

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+const init = async () => {
+
+    await server.register(require('inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return h.file('./index.html');
+        }
+    });
+    
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: (request, h) => {
+
+            return h.file(`./${request.params.param}`);
+        }
+    });
+    
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
